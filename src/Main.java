a5bd9312c632b23157c1e17fe930a9785340afe7
Bhoraj1 <journeyn556@gmail.com>
2025-10-31 00:27:50 +0545

    today completion of day1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public  static void main(String[] args){
-    System.out.println("Hello World");
+     int[] numberss = {2,3,4,5,6,7};
+        Arrays.sort(numberss);
+        String results = Arrays.toString(numberss);
+        System.out.println(results);
+
+        // Anotehr
+        int[] numbers = new int[5];
+        numbers[0]=1;
+        numbers[1]=2;
+        String result =  Arrays.toString(numbers);
+        System.out.println(result);
     }
 }
+
+
